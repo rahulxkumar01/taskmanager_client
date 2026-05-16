2d29e34cde54dcd05773d01cd2c38d0f6dc739b8
Rahul Kumar <rahulkumar99538@gmail.com>
2026-05-16 06:23:47 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': 'http://localhost:5000',
+    },
+  },
+  preview: {
+    port: process.env.PORT || 4173,
+    host: '0.0.0.0',
+  },
+});
